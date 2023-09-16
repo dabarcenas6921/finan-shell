4ae4219e504a9c2cb497b520d8c785e5e8ee77c4
David Barcenas <dabarcenas6921@gmail.com>
2023-09-16 18:59:06 -0400

    modules finished

---
+++ src/app/_components/sidebar.tsx
@@ -0,0 +1,96 @@
+import Link from "next/link";
+import LessonContent from "./lessonContent";
+import { ModuleData } from "./lessonContent";
+
+const lessonsWithCategories = [
+  {
+    category: "Finance Management",
+    lessons: [
+      {
+        href: "/lessons/budgeting-and-expense-tracking",
+        text: "Budgeting and Expense Tracking",
+      },
+    ],
+  },
+  {
+    category: "Banking",
+    lessons: [
+      {
+        href: "/lessons/banking-basics",
+        text: "Banking Basics",
+      },
+    ],
+  },
+  {
+    category: "Investing",
+    lessons: [
+      {
+        href: "/lessons/investment-fundamentals",
+        text: "Investment Fundamentals",
+      },
+    ],
+  },
+  {
+    category: "Planning",
+    lessons: [
+      {
+        href: "/lessons/retirement-planning",
+        text: "Retirement Planning",
+      },
+      {
+        href: "/lessons/financial-goals-and-planning",
+        text: "Financial Goals and Planning",
+      },
+    ],
+  },
+  {
+    category: "Credit",
+    lessons: [
+      {
+        href: "/lessons/credit-building",
+        text: "Credit Building Tips and Tricks",
+      },
+    ],
+  },
+];
+
+export default function Sidebar({
+  moduleContent,
+}: {
+  moduleContent: ModuleData;
+}) {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content ml-6 flex w-full flex-col items-start md:w-3/4">
+        {/* Page content here */}
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+        <LessonContent moduleData={moduleContent} />
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          {lessonsWithCategories.map((categoryItem, index) => (
+            <div key={index}>
+              <h2 className="mb-2 text-lg font-semibold">
+                {categoryItem.category}
+              </h2>
+              <ul>
+                {categoryItem.lessons.map((lesson, lessonIndex) => (
+                  <li key={lessonIndex} className="mb-2">
+                    <Link href={lesson.href}>{lesson.text}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/lessons/credit-building/page.tsx
@@ -0,0 +1,27 @@
+import Sidebar from "~/app/_components/sidebar";
+
+export default function creditBuilding() {
+  return <div>{<Sidebar moduleContent={content} />}</div>;
+}
+
+let content = {
+  heading: "Credit Building Tips and Tricks",
+  subcontent: [
+    {
+      subheading: "Understanding Credit Scores",
+      paragraph:
+        "Welcome to Module 6: Credit Building Tips and Tricks. In this section, we will explore the world of credit and how to build and maintain a healthy credit score. Understanding credit scores is vital for financial success. Let's dive into the details:\n\n1. **What is a Credit Score**: Learn what a credit score is and why it matters. Understand how credit scores are calculated and the factors that influence them.\n\n2. **Importance of Good Credit**: Explore the importance of having a good credit score. A strong credit history can open doors to better financial opportunities, such as lower interest rates on loans.\n\n3. **Checking Your Credit Report**: Discover how to access your credit reports from the major credit bureaus. Regularly review your reports for errors and discrepancies.\n\n4. **Factors Affecting Credit**: Understand the key factors that impact your credit score, including payment history, credit utilization, length of credit history, and types of credit.\n\n5. **Improving Your Credit Score**: Explore strategies for improving your credit score, such as paying bills on time, reducing credit card balances, and addressing negative items on your credit report.\n\nBy the end of this module, you'll have a comprehensive understanding of credit scores and be on your way to building and maintaining excellent credit.",
+    },
+    {
+      subheading: "Building and Managing Credit",
+      paragraph:
+        "Building and managing credit requires a strategic approach. Consider these credit-building tips and tricks:\n\n1. **Secured Credit Cards**: If you have limited or poor credit history, consider applying for a secured credit card. These cards require a security deposit but can help establish positive credit history.\n\n2. **Credit Utilization**: Understand the importance of credit utilization. Aim to keep your credit card balances below 30% of your credit limit to positively impact your credit score.\n\n3. **Payment History**: Always make payments on time. Late payments can have a significant negative impact on your credit score.\n\n4. **Credit Mix**: Diversify your credit mix by having a combination of installment loans (e.g., student loans) and revolving credit (e.g., credit cards).\n\n5. **Credit Monitoring**: Consider using credit monitoring services or apps to stay informed about changes to your credit score and any potential identity theft.\n\n6. **Credit Repair**: If you have errors or negative items on your credit report, explore credit repair strategies to address and dispute inaccurate information.\n\nBy the end of this section, you'll have a toolkit of credit-building tips and tricks to help you strengthen your credit profile.",
+    },
+    {
+      subheading: "Responsible Credit Use",
+      paragraph:
+        "Responsible credit use is the foundation of a healthy credit history. Explore these practices:\n\n1. **Budgeting**: Create a budget that includes your credit card payments. Avoid charging more than you can afford to pay off each month.\n\n2. **Automatic Payments**: Set up automatic payments for your credit cards to ensure you never miss a due date.\n\n3. **Avoiding Minimum Payments**: Whenever possible, pay more than the minimum payment on your credit cards to reduce interest charges.\n\n4. **Credit Limits**: Be mindful of your credit limits and avoid maxing out your credit cards.\n\n5. **Closing Accounts**: Think carefully before closing old credit card accounts, as this can impact your credit history length.\n\n6. **Credit Counseling**: If you're struggling with credit card debt, consider credit counseling services for debt management strategies.\n\nBy the end of this module, you'll have the knowledge and practices to use credit responsibly and build a strong credit history.",
+    },
+  ],
+  video: "https://www.youtube.com/embed/dW77CWoUW_Q?si=GJDxh7ZhFB14y20y",
+};

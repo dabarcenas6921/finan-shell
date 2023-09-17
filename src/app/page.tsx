aecac74f0daa3803d121d29bd49e3aae08fc650c
David Barcenas <dabarcenas6921@gmail.com>
2023-09-17 07:07:40 -0400

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,62 @@
+"use client";
+import Spline from "@splinetool/react-spline";
 import Link from "next/link";
-import { api } from "~/trpc/server";
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <Link href="/sign-up">Sign Up</Link>
-        <Link href="/sign-in">Sign In</Link>
+    <div className="flex h-screen -translate-y-16 transform items-center justify-center">
+      {" "}
+      {/* Added transform and translate utilities */}
+      {/* Left Column: Text */}
+      <div className="mx-8 w-1/2 text-center">
+        <h1
+          className="leading-tighter mb-4 text-4xl font-extrabold tracking-tighter text-white md:text-7xl"
+          data-aos="zoom-y-out"
+        >
+          Unlock your financial{" "}
+          <span className="bg-gradient-to-r from-[#641AE6] to-[#FFE2F5] bg-clip-text text-transparent">
+            freedom
+          </span>
+        </h1>
+
+        <div className="mx-auto max-w-3xl">
+          <p
+            className="mb-8 text-xl text-gray-400"
+            data-aos="zoom-y-out"
+            data-aos-delay="150"
+          >
+            With FinanShell, take control of your expenses, budget like a pro,
+            and learn the secrets of successful investing from our community.
+          </p>
+
+          <div
+            className="mx-auto mb-4 max-w-xs sm:flex sm:max-w-none sm:justify-center"
+            data-aos="zoom-y-out"
+            data-aos-delay="300"
+          >
+            <div>
+              <Link href="/sign-up">
+                <button className="btn btn-primary mb-4 w-full text-white hover:bg-blue-700 sm:mb-0 sm:w-auto">
+                  Register
+                </button>
+              </Link>
+            </div>
+            <div>
+              <Link href="/sign-in">
+                <button className="btn btn-secondary w-full text-white hover:bg-gray-800 sm:ml-4 sm:w-auto">
+                  Login
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-    </main>
+      {/* Right Column: Spline Object */}
+      <div className="flex w-1/2 items-center justify-center">
+        <div className="h-screen w-screen">
+          <Spline scene="https://prod.spline.design/tGhfhJAi87C1Sz5s/scene.splinecode" />
+        </div>
+      </div>
+    </div>
   );
 }
